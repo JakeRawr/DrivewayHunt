92d0fe474c3eb96ad1773a35448a53756849b663
Jake Yang <tsunghan614@gmail.com>
2014-12-17 15:58:45 -0800

    fix sale select row to a title link

--- app/js/directives/search_results_directive.js
+++ app/js/directives/search_results_directive.js
@@ -7,12 +7,13 @@ module.exports = function(app) {
       transclude: true,
       templateUrl: 'js/templates/search_results.html',
       link: function($scope) {
-        $scope.relocate = function(lat, lng) {
-          $scope.mark = {
-            loc: {latitude: lat, longitude: lng}
-          };
+        $scope.relocate = function($event, lat, lng) {
+          if (angular.element($event.target)[0].id !== "redirectSale"){
+            $scope.mark = {
+              loc: {latitude: lat, longitude: lng}
+            };
+          }
         };
-        //$scope.mark = $scope.sales[0].loc;
       }
     };
   });
