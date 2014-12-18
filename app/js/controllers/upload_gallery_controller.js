bc1d6f97ae79047729e2fa43badd65d58ac6920c
Gabe Medrash <gmedrash@gmail.com>
2014-12-18 14:46:38 -0800

    fix image upload

--- app/js/controllers/upload_gallery_controller.js
+++ app/js/controllers/upload_gallery_controller.js
@@ -13,19 +13,21 @@ module.exports = function(app) {
       $scope.itemModalShown = false;
     });
 
+    $scope.image = {};
+
     $scope.upload = function(image) {
       if (!image) return;
       $upload.upload({
         url: 'https://api.cloudinary.com/v1_1/dqwea7i3j/upload',
         data: {upload_preset: 'osxh5dpi'},
         file: image
-      }).progress(function(e) {
-        console.log('e' + e);
-      }).success(function(data, status, headers, config) {
-        console.log('data ', data);
-        console.log('status ', status);
-        console.log('headers', headers);
-        console.log('config', config);
+      }).progress(function() {
+
+      }).success(function(data) {
+        $scope.image.url = data.url;
+        var splitUrl = $scope.image.url.split('upload/');
+        $scope.image.url = splitUrl[0] + 'upload/w_200,h_200/' + splitUrl[1];
+        $scope.image.alt = data.public_id;
         $rootScope.$broadcast(AUTH_EVENTS.itemEditAttempt);
       });
     };
@@ -35,11 +37,8 @@ module.exports = function(app) {
     };
 
     $scope.save = function(title, description, condition, url) {
-      console.log(title);
-      console.log(description);
-      console.log(condition);
-      console.log(url);
       //call service
+      console.log(title, description, condition, url);
       $rootScope.$broadcast(AUTH_EVENTS.itemEditFinished);
     };
 
